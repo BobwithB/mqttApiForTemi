512de3b7e9646d03f7b1e7ea859542776b8ab9a5
韦德波 <weidebo@coocaa.com>
2019-11-01 19:57:46 +0800

    ..
    Signed-off-by: 韦德波 <weidebo@coocaa.com>

--- mqttApiServer/test/test1.js
+++ mqttApiServer/test/test1.js
@@ -1,7 +1,7 @@
 var request = require('superagent');
 var expect = require('chai').expect;
 
-describe('async.test.js ', function() {
+describe('test/test1.js ', function() {
     it('get allReqLog', function(done){
         request
             .get('http://localhost:8082/reqLog/allReqLog?aa=ee&dd=bb')
