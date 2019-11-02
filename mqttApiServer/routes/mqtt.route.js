64890ba0b5175df30f522a85205c6e9cee4527e4
韦德波 <weidebo@coocaa.com>
2019-11-03 02:20:44 +0800

    add a retrieve api:get all topics
    Signed-off-by: 韦德波 <weidebo@coocaa.com>

--- mqttApiServer/routes/mqtt.route.js
+++ mqttApiServer/routes/mqtt.route.js
@@ -1,9 +1,26 @@
 import express from "express";
+import request from "request";
 import mqttService from "../service/mqtt.service"
 const router = express.Router()
 
 router.post('/publish', (req, res) => {
     mqttService.sendMsg(req, res);
+});
+router.post('/topics', (req, res) => {
+
+    request.get('https://customer.cloudmqtt.com/api/instances', {
+        'auth': {
+            'user': '',
+            'pass': '79fcbec9-77de-43b4-a63a-fce628bce76d',
+        }
+    }, function (err, httpResponse, data) {
+        if (err) {
+            console.log(err);
+        } else {
+            console.log(`topics:${data}`)
+            res.send(data);
+        }
+    })
 });
 
 export default router;
