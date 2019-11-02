1c32035aa84449ff9f29d41da3bcfcb88529d392
韦德波 <weidebo@coocaa.com>
2019-11-02 17:25:14 +0800

    add mqtt a demo first
    Signed-off-by: 韦德波 <weidebo@coocaa.com>

--- mqttApiServer/models/reqLog.model.js
+++ mqttApiServer/models/reqLog.model.js
@@ -1,5 +1,5 @@
 import mongoose from 'mongoose';
-
+import mqtt from'mqtt';
 const reqLogSchema = mongoose.Schema({
     req: Object
 }, {collection : 'logs'});
@@ -7,6 +7,25 @@ const reqLogSchema = mongoose.Schema({
 const reqLogModel = mongoose.model('logs', reqLogSchema);
 
 reqLogModel.getAll = () => {
+
+    var mqtt_url = 'mqtt:tailor.cloudmqtt.com';
+    var topic = 'myCloudMQTT';
+    var client = mqtt.connect(mqtt_url,{port:'18558',username:'fusdnbdl',password:'J4A79NQv35ke'});
+
+    client.on('connect', function () {
+        client.subscribe(topic, function (err) {
+
+            if (!err) {
+                client.publish(topic, 'Hello mqtt')
+            }
+        })
+    })
+
+    client.on('message', function (topic, message) {
+        console.log(message.toString())
+        client.end()
+    })
+
     return reqLogModel.find({});
 }
 reqLogModel.inserLogs = (logs) => {
